e76d016a08ca508b89d318cfd827e752ffc60ffe
Barry550 <mamadoubarry6059@gmail.com>
2024-02-07 20:49:58 +0000

    deploy

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function page() {
-  return <div>contact</div>;
+  return (
+    <div className="mx-5">
+      contact Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+      laboriosam consequatur recusandae vel a vero quo qui facere! In minus
+      architecto reiciendis aliquam eos numquam quod facilis doloribus, amet
+      fugiat.
+    </div>
+  );
 }
